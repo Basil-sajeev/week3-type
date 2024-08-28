357972d3f27f5822b5a1bbfc751c6bf06c16b13a
Basil-sajeev <basilsajeev360@gmail.com>
2024-08-28 12:17:41 +0530

    first commit

--- components/main/locations/locations.tsx
+++ components/main/locations/locations.tsx
@@ -10,11 +10,11 @@ const Locations : React.FC= () => {
      <h5>Locations</h5>
 <div className={styles.loc1}>
 <ul>
-     <div className={styles.loc11}><div className={styles.loc111}><Image src="images/maps.svg"   width={15} height={60} alt={'map1'}/><p>Dubai</p></div></div>
-     <div className={styles.loc11}><div className={styles.loc111}><Image src="images/maps.svg" width={15} height={60} alt={'map2'}/><p>Abu Dhabi</p></div></div>
-     <div className={styles.loc11}><div className={styles.loc111}><Image src="images/maps.svg" width={15} height={60} alt={'map3'}/><p>Sharjah</p></div></div>
-     <div className={styles.loc11}><div className={styles.loc111}><Image src="images/maps.svg" width={15} height={60}alt={'map4'} /><p>Fujairah</p></div></div>
-     <div className={styles.loc11}><div className={styles.loc111}><Image src="images/maps.svg"   width={15} height={60}alt={'map5'} /><p>Ras Al Khaimah</p></div></div>
+     <div className={styles.loc11}><div className={styles.loc111}><Image src="/images/maps.svg"   width={15} height={60} alt={'map1'}/><p>Dubai</p></div></div>
+     <div className={styles.loc11}><div className={styles.loc111}><Image src="/images/maps.svg" width={15} height={60} alt={'map2'}/><p>Abu Dhabi</p></div></div>
+     <div className={styles.loc11}><div className={styles.loc111}><Image src="/images/maps.svg" width={15} height={60} alt={'map3'}/><p>Sharjah</p></div></div>
+     <div className={styles.loc11}><div className={styles.loc111}><Image src="/images/maps.svg" width={15} height={60}alt={'map4'} /><p>Fujairah</p></div></div>
+     <div className={styles.loc11}><div className={styles.loc111}><Image src="/images/maps.svg"   width={15} height={60}alt={'map5'} /><p>Ras Al Khaimah</p></div></div>
      </ul>
  </div>
  
